69292907584cb10355671e84097e13cd48858d7b
Sasi Kumar M <sasikumarmurugan02@gmail.com>
2023-02-24 22:46:19 +0530

    12

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
-import React from "react";
-// import Article from "./components/article/Article";
-// import Brand from "./components/brand/Brand";
-// import Cta from "./components/cta/Cta";
-// import Feature from "./components/feature/Feature";
-// import Navbar from "./components/navbar/Navbar";
-import './App.css';
-import { Footer,Blog, Possibility, Features, WhatGPT3, Header } from "./containers";
-import { Article, Brand, Cta, Feature, Navbar } from "./components";
+import React from 'react';
 
-const App = () => { //creating a function name / components as App for the App.js
-    return (
-        <div className="App">
-            <div className="gradient__bg">
-                <Navbar/>
-                <Header/>
-                <Article/>
-                <Feature/>
-            </div>
-            <Brand/>
-            <WhatGPT3/>
-            <Features/>
-            <Possibility/>
-            <Cta/>
-            <Blog/>
-            <Footer/>
-        </div>
-    );
-}
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
+
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
+    </div>
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-// importing the react and allows us to took this react application to index.html 
-import React from "react";
+import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root')) // render the application by saying document.getElementById that's going to be root
+import App from './App';
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
